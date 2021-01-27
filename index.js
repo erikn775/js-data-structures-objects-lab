bb7bc3a33621136382acedc00d69ee6e231ec09d
erikn775 <erikn775@gmail.com>
2021-01-27 11:49:13 -0700

    commit tree 58ed0a54e7b461c14f0884c65554857e83daf112

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your solution in this file!
+let driver = {};
+
+function updateDriverWithKeyAndValue(driver, key, value) {
+    let newDriver = driver;
+    newDriver.name = 'Sam';
+    newDriver.address = '11 Broadway';
+    return newDriver
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+
+}
